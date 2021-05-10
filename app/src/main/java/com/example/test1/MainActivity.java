486065b9ac0c3398571116a1965f9126eb59b600
faith <1422703894@qq.com>
2021-05-10 16:47:10 +0800

    第一次本地提交到远程

---
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ *
+ * @author： faith
+ * @Time： 2021/5/10 16:27
+ * @EditVersion：2.0.1
+ * @description： 项目描述
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
